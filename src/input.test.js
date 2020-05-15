ee477be548ef3e8c6f4af35da7eefb4d1d08b8e0
Andy Durette <duretteandy@gmail.com>
2020-05-15 18:34:07 -0400

    Complete Input component render tests for success=false

--- src/input.test.js
+++ src/input.test.js
@@ -19,28 +19,33 @@ const setup = (initialState={}) => {
 
 describe('render', () => {
   describe('word has not been guessed', () => {
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { success: false};
+      wrapper = setup(initialState);
+    })
+    test('renders component without error', () => {
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
+    });
+    test('renders input box', () => {
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      expect(inputBox.length).toBe(1);
+    });
+    test('renders a submit button', () => {
+      const submitButton = findByTestAttr(wrapper, "submit-button");
+      expect(submitButton.length).toBe(1);
+    });
+  });
+  describe('word has been successfully guessed', () => {
     test('renders component without error', () => {
 
     });
-    test('renders input box', () => {
+    test('does not render input box', () => {
 
     });
-    test('renders a submit button', () => {
+    test('does not render a submit button', () => {
 
     });
-  });
-  describe('word has been guessed', () => {
-    
-  });
-});
-describe('update state', () => {
-  test('renders component without error', () => {
-
-  });
-  test('does not render input box', () => {
-
-  });
-  test('does not render a submit button', () => {
-
   });
 });
