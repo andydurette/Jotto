4287e5a480687001898be686e0d1ba818b983c98
Andy Durette <duretteandy@gmail.com>
2020-05-19 17:46:33 -0400

    Add guessWords piece of state

--- src/reducers/guessedWordsReducer.js
+++ src/reducers/guessedWordsReducer.js
@@ -1,3 +1,5 @@
+import { actionTypes } from '../actions';
+
 /**
  * @function guessWordsReducer
  * @param {array} state - Array og guessed words.
@@ -5,6 +7,11 @@
  * @returns {array} - new guessedWords state.
  */
 
-export default (state, action) => {
-  return null;
+export default (state=[], action) => {
+  switch(action.type) {
+    case actionTypes.GUESS_WORD:
+      return [...state, action.payload]
+    default:
+      return state;
+  }
 }
