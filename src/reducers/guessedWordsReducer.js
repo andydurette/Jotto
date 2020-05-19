0db003c8094cded2b35cad3e00eb481827532480
Andy Durette <duretteandy@gmail.com>
2020-05-19 13:44:56 -0400

    Set up guessWords tests

---
+++ src/reducers/guessedWordsReducer.js
@@ -0,0 +1,10 @@
+/**
+ * @function guessWordsReducer
+ * @param {array} state - Array og guessed words.
+ * @param {object} action - action to be reduced.
+ * @returns {array} - new guessedWords state.
+ */
+
+export default (state, action) => {
+  return null;
+}
