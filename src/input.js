277937121f2fe1576091871980c19d0456226e7e
Andy Durette <duretteandy@gmail.com>
2020-05-27 23:29:15 -0400

    Added simulate event for form field test

--- src/input.js
+++ src/input.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function Input(props) {
+function Input({ secretWord }) {
+  const [ currentGuess, setCurrentGuess ] = React.useState("");
   return (
     <div data-test='component-input'>
-      
+      <form className="form-inline">
+        <input 
+        data-test="input-box" 
+        type="text"
+        className="mb-2 mx-sm-3"
+        type="text"
+        placeholder="enter guess"
+        value={currentGuess}
+        onChange={(event) => setCurrentGuess(event.target.value)}
+        />
+        <button
+          datta-test="submit-button"
+          className="btn btn-primary mb-2">
+          Submit
+        </button>
+      </form>
     </div>
   )
+}
+
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired,
 }
 
 export default Input
